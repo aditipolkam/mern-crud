8aa62cd74330d96065df3beeafdec5a3549d859d
Aditi Polkam <aditipolkam@gmail.com>
2022-06-07 09:19:02 +0530

    [ADD] navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <NavBar/>
     </div>
   );
 }
