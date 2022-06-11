476cb5ce985eed692c098f04a4b451ed9e483b37
Aditi Polkam <aditipolkam@gmail.com>
2022-06-11 12:57:08 +0530

    [REMOVE] procfile

--- server/app.js
+++ server/app.js
@@ -4,13 +4,12 @@ require('./db/conn');
 require("dotenv").config();
 const users = require('./models/userSchema');
 const router = require("./routes/router");
-const port = 8003;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(process.env.PORT || port, () => {
+app.listen(process.env.PORT || 8003, () => {
     console.log(`Server is running on port ${port}`);
 })
