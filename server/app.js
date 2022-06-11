e5226b36574de67df9eb8f4ff9b012c1f574914a
Aditi Polkam <aditipolkam@gmail.com>
2022-06-11 12:59:41 +0530

    [FIX] log port error

--- server/app.js
+++ server/app.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(router);
 
 app.listen(process.env.PORT || 8003, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running`);
 })
